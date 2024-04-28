cb0fdf329bb889d5fc577bd211c13c6adb23692c
Anton Derevnin <lyaguxmaslo@gmail.com>
2024-04-28 16:15:45 +0300

    Updated Employee entity

--- Database/Entities/Employee.cs
+++ Database/Entities/Employee.cs
@@ -4,21 +4,14 @@ namespace Database.Entities
 {
     public class Employee : BaseEntity
     {
-        public string FirstName { get; set; } = null!;
-
-        public string? MiddleName { get; set; }
-
-        public string LastName { get; set; } = null!;
-
-        public string Phone { get; set; } = null!;
-
-        public string Login { get; set; } = null!;
-
-        public string Password { get; set; } = null!;
-
         [ForeignKey(nameof(Position))]
         public int PositionId { get; set; }
 
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+
         public Position Position { get; set; }
+
+        public User User { get; set; }
     }
 }
